8bcd70dd09d0c3974dd6ca94756e73556a449aef
Antoine LE FRAPPER <antoine.lefrapper@omexom.com>
2024-11-14 12:09:31 +0100

    validations fait

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
+  # Associations
   belongs_to :movie
   belongs_to :list
+
+  # Validations
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "has already been added to this list" }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,8 @@
 class List < ApplicationRecord
+    # Associations
+    has_many :bookmarks, dependent: :destroy
+    has_many :movies, through: :bookmarks
+
+    # Validation
+    validates :name, presence: true, uniqueness: true
 end
